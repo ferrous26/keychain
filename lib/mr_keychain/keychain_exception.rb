3412e498f1fe2f11d05b4376ffab0b391cc6b26e
Mark Rada <mrada@marketcircle.com>
2011-01-30 22:31:21 -0500

    Use fewer colons

--- lib/mr_keychain/keychain_exception.rb
+++ lib/mr_keychain/keychain_exception.rb
@@ -9,7 +9,7 @@ class KeychainException < Exception
   # @param [Fixnum] error_code the result code from calling a Sec function
   def initialize message_prefix, error_code
     cf_message = SecCopyErrorMessageString( error_code, nil )
-    super "#{message_prefix}: #{cf_message}"
+    super "#{message_prefix}. #{cf_message}"
   end
 
 end
