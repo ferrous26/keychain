e0b98b39f33169e32b59711e2831e927a937b990
Mark Rada <mrada@marketcircle.com>
2011-02-04 00:52:54 -0500

    Fill in pending specs for Keychain.lookup_item

--- spec/keychain_spec.rb
+++ spec/keychain_spec.rb
@@ -52,13 +52,61 @@ describe Keychain do
 
 
   describe '.lookup_item' do
-    it 'should return nil if nothing is found'
-    it 'should return a single item for singular search'
-    it 'should return an array of items for plural search'
-    it 'should allow you to override the match limit'
-    it 'should ignore any extra return type key/value pairs'
-    it 'should not mutate the given search dictionary'
-    it 'should ignore any extra return type key/value pairs'
+    it 'should return nil if nothing is found' do
+      result = Keychain.lookup_item(
+                                    KSecAttrProtocol => KSecAttrProtocolIRCS,
+                                    KSecAttrServer   => 'github.com'
+                                    )
+      result.should be_nil
+    end
+
+    it 'should return a single item for singular search' do
+      result = Keychain.lookup_item(
+                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
+                                    KSecAttrServer   => 'github.com'
+                                    )
+      result.class.should == Keychain::Item
+    end
+
+    it 'should return an array of items for plural search' do
+      result = Keychain.lookup_item(
+                                    KSecMatchLimit   => KSecMatchLimitAll,
+                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
+                                    KSecAttrServer   => 'github.com'
+                                    )
+      result.class.should == Array
+      result.first.class.should == Keychain::Item
+    end
+
+    it 'should allow you to override the match limit' do
+      result = Keychain.lookup_item(
+                                    KSecMatchLimit   => KSecMatchLimitAll,
+                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
+                                    )
+      result.class.should == Array
+    end
+
+    it 'should ignore any extra return type key/value pairs' do
+      expect {
+        Keychain.lookup_item(
+                             KSecAttrProtocol        => KSecAttrProtocolHTTPS,
+                             KSecAttrServer          => 'github.com',
+                             KSecReturnData          => true,
+                             KSecReturnRef           => true,
+                             KSecReturnPersistentRef => true
+                             )
+      }.to_not raise_error Keychain::KeychainException
+    end
+
+    it 'should not mutate the given search dictionary' do
+      search_dict = {
+        KSecAttrProtocol => KSecAttrProtocolIRCS,
+        KSecAttrServer   => 'github.com'
+      }
+      original_dict = search_dict.dup
+      Keychain.lookup_item search_dict
+      search_dict.should == original_dict
+    end
   end
 
 end
