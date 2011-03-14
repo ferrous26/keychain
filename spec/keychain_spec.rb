3f8e03b142218c1201b084d868baf74eb3a86cd8
Mark Rada <mrada@marketcircle.com>
2011-03-14 01:50:15 -0400

    Rewrite keychain_spec

--- spec/keychain_spec.rb
+++ spec/keychain_spec.rb
@@ -3,113 +3,89 @@ require 'spec_helper'
 describe Keychain do
 
   describe '.item_exists?' do
-
     it 'returns false if the item does not exist' do
-      Keychain.item_exists?(
-        KSecAttrProtocol => KSecAttrProtocolIRCS,
-        KSecAttrServer   => 'github.com'
-      ).should == false
+      Keychain.item_exists?( KSecAttrProtocol => KSecAttrProtocolIRCS,
+                             KSecAttrServer   => 'github.com' ).should be_false
     end
-
-    it 'returns true if the item exists' do
-      Keychain.item_exists?(
-        KSecAttrProtocol => KSecAttrProtocolHTTPS,
-        KSecAttrServer   => 'github.com'
-      ).should == true
+    it 'returns false if there is an unexpected problem' do
+      Keychain.item_exists?( KSecClass => 'fake class name' ).should be_false
     end
-
-    it 'raise an exception for unexpected error codes' do
-      expect {
-        Keychain.item_exists?(KSecClass => 'fake')
-      }.to raise_exception Keychain::KeychainException
+    it 'returns true if at least one item matches' do
+      Keychain.item_exists?( KSecAttrProtocol => KSecAttrProtocolHTTPS,
+                             KSecAttrServer   => 'github.com' ).should be_true
     end
-
+    it 'returns true if there are multiple matches' do
+      Keychain.item_exists?( KSecAttrProtocol => KSecAttrProtocolHTTPS ).should be_true
+    end
     it 'should not mutate the given search dictionary' do
-      search_dict = {
-        KSecAttrProtocol => KSecAttrProtocolIRCS,
-        KSecAttrServer   => 'github.com'
-      }
-      original_dict = search_dict.dup
+      search_dict = (original_dict = { KSecAttrServer => 'github.com' }).dup
       Keychain.item_exists? search_dict
       search_dict.should == original_dict
     end
-
-    # I think this might be impossible to test without human intervention
-    # It also passes right now even though it should not.falsely
-    it 'should ignore any extra return type key/value pairs' do
-      expect {
-        Keychain.item_exists?(
-          KSecAttrProtocol => KSecAttrProtocolHTTPS,
-          KSecAttrServer   => 'github.com',
-          KSecReturnData   => true
-        )
-      }.to_not raise_error Keychain::KeychainException
-    end
-
-    it 'should allow for additional search attribute key/vaule pairs'
-    it 'should allow the item class to be overriden'
+    it 'should allow the class to be overriden'
+    it 'should allow filtering based on attribute key/value pairs'
+    it 'should allow filtering based on search key/value pairs'
+    it 'should ignore any extra return type key/value pairs'
   end
 
-
-  describe '.lookup_item' do
+  describe '.item' do
     it 'should return nil if nothing is found' do
-      result = Keychain.lookup_item(
-                                    KSecAttrProtocol => KSecAttrProtocolIRCS,
-                                    KSecAttrServer   => 'github.com'
-                                    )
-      result.should be_nil
+      Keychain.item( KSecAttrServer => 'fake.example.org' ).should be_nil
     end
-
-    it 'should return a single item for singular search' do
-      result = Keychain.lookup_item(
-                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
-                                    KSecAttrServer   => 'github.com'
-                                    )
-      result.class.should == Keychain::Item
+    it 'should return a single item if a single item is found' do
+      ret = Keychain.item KSecAttrServer => 'github.com'
+      ret.should_not be_nil
+      ret.class.should == Keychain::Item
+      ret[KSecAttrServer].should == 'github.com'
     end
-
-    it 'should return an array of items for plural search' do
-      result = Keychain.lookup_item(
-                                    KSecMatchLimit   => KSecMatchLimitAll,
-                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
-                                    KSecAttrServer   => 'github.com'
-                                    )
-      result.class.should == Array
-      result.first.class.should == Keychain::Item
-    end
-
-    it 'should allow you to override the match limit' do
-      result = Keychain.lookup_item(
-                                    KSecMatchLimit   => KSecMatchLimitAll,
-                                    KSecAttrProtocol => KSecAttrProtocolHTTPS,
-                                    )
-      result.class.should == Array
-    end
-
-    it 'should ignore any extra return type key/value pairs' do
+    it 'should raise an error in case of an unexpected result code' do
       expect {
-        Keychain.lookup_item(
-                             KSecAttrProtocol        => KSecAttrProtocolHTTPS,
-                             KSecAttrServer          => 'github.com',
-                             KSecReturnData          => true,
-                             KSecReturnRef           => true,
-                             KSecReturnPersistentRef => true
-                             )
-      }.to_not raise_error Keychain::KeychainException
+        Keychain.item( KSecClass => 'fake class name' )
+      }.to raise_error Keychain::KeychainException
     end
-
     it 'should not mutate the given search dictionary' do
-      search_dict = {
-        KSecAttrProtocol => KSecAttrProtocolIRCS,
-        KSecAttrServer   => 'github.com'
-      }
-      original_dict = search_dict.dup
-      Keychain.lookup_item search_dict
+      search_dict = (original_dict = { KSecAttrServer => 'github.com' }).dup
+      Keychain.item search_dict
       search_dict.should == original_dict
     end
+    it 'should allow the class to be overridden'
+    it 'should allow filtering based on attribute key/value pairs'
+    it 'should allow filtering based on search key/value pairs'
+    it 'should ignore any additional return type keys'
+  end
 
-    it 'should allow for additional search attribute key/vaule pairs'
-    it 'should allow the item class to be overriden'
+  describe '.items' do
+    it 'should return an empty array if nothing is found' do
+      Keychain.items( KSecAttrServer => 'fake.example.org' ).should be_nil
+    end
+    it 'should return an array of one if a single item is found' do
+      ret = Keychain.items KSecAttrServer => 'github.com'
+      ret.should_not be_nil
+      ret.class.should == Array
+      ret.first[KSecAttrServer].should == 'github.com'
+      ret.size.should satisfy { |size| size > 1 }
+    end
+    it 'should return many items if many items are found' do
+      ret = Keychain.items( KSecAttrProtocol => KSecAttrProtocolHTTPS )
+      ret.should_not be_nil
+      ret.class.should == Array
+      ret.first.class.should == Keychain::Item
+      ret.first[KSecAttrProtocol].should == KSecAttrProtocolHTTPS
+    end
+    it 'should raise an error in case of an unexpected result code' do
+      expect {
+        Keychain.items( KSecClass => 'fake class name' )
+      }.to raise_error Keychain::KeychainException
+    end
+    it 'should not mutate the given search dictionary' do
+      search_dict = (original_dict = { KSecAttrServer => 'github.com' }).dup
+      Keychain.items search_dict
+      search_dict.should == original_dict
+    end
+    it 'should allow the class to be overridden'
+    it 'should allow filtering based on attribute key/value pairs'
+    it 'should allow filtering based on search key/value pairs'
+    it 'should ignore any additional return type keys'
   end
 
 end
