1611d6facfb948c5b291c9a8ba8ea0c43859829a
Mark Rada <mrada@marketcircle.com>
2011-03-13 20:51:17 -0400

    Update the KeychainException specs

--- spec/keychain_exception_spec.rb
+++ spec/keychain_exception_spec.rb
@@ -1,27 +1,17 @@
 require 'spec_helper'
 
-describe Keychain::Exception do
-
-  describe '#initialize' do
-    it 'should take an error code as a Fixnum' do
-      expect { Keychain::KeychainException.new 'test', 0 }.to_not raise_error ArgumentError
-      expect { Keychain::KeychainException.new 'test', 0 }.to_not raise_error TypeError
-    end
-
-    it 'should take a result code constant' do
-      expect { Keychain::KeychainException.new 'test', ErrSecSuccess }.to_not raise_error ArgumentError
-      expect { Keychain::KeychainException.new 'test', ErrSecSuccess }.to_not raise_error TypeError
-    end
-
-    it 'should look up the error code for the error message' do
-      exception = Keychain::KeychainException.new 'test', ErrSecUnimplemented
-      exception.message.should match /not implemented/
-    end
-
-    it 'should take a message prefix' do
-      exception = Keychain::KeychainException.new 'my prefix', 0
-      exception.message.should match /^my prefix/
-    end
+describe Keychain::Exception, '#initialize' do
+  it 'should take a message prefix and an error code' do
+    Keychain::KeychainException.instance_method(:initialize).arity.should be 2
+    expect { Keychain::KeychainException.new '', ErrSecSuccess }.to_not raise_error ArgumentError
+    expect { Keychain::KeychainException.new '', ErrSecSuccess }.to_not raise_error TypeError
   end
-
+  it 'should add the prefix to beginning of the error message'
+    exception = Keychain::KeychainException.new 'my prefix', 0
+    exception.message.should match /^my prefix/
+  end
+  it 'should look up the error code for the error message' do
+    exception = Keychain::KeychainException.new 'test', ErrSecUnimplemented
+    exception.message.should match /not implemented/
+  end
 end
