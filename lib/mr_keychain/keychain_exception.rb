a3ffcb04ca512aad445fcbf071a34aad981be398
Mark Rada <mrada@marketcircle.com>
2011-03-13 20:42:44 -0400

    More documentation tweaks

--- lib/mr_keychain/keychain_exception.rb
+++ lib/mr_keychain/keychain_exception.rb
@@ -1,17 +1,16 @@
 module Keychain
 
-# A trivial exception class that exists to help differentiate where
-# exceptions are being raised.
+##
+# Properly extracts error messages based on error codes.
 class KeychainException < Exception
 
-  # @param [#to_s] message_prefix meant to be an indicator of where the
-  #  exception originated from
-  # @param [Fixnum] error_code the result code from calling a Sec function
+  ##
+  # @param [#to_s] message_prefix an indicator of where the exception originated
+  # @param [Fixnum] error_code result code from calling a Sec function
   def initialize message_prefix, error_code
     cf_message = SecCopyErrorMessageString( error_code, nil )
     super "#{message_prefix}. #{cf_message}"
   end
 
 end
-
 end
