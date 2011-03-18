ed48672ba8f9a7b21590c10d523183b533922d74
Mark Rada <mrada@marketcircle.com>
2011-03-18 01:43:21 -0400

    Rewrite Keychain.item_exists? moving some stuff to a helper
    So that other methods can also use the same idea without
    introducing duplication.

--- lib/mr_keychain/keychain.rb
+++ lib/mr_keychain/keychain.rb
@@ -2,47 +2,38 @@ module Keychain
 
   class << self
 
-    # @note This method asks only for the metadata and doesn't need
-    #  user authorization
+    ##
+    # @note This method asks only for the metadata and should never need
+    #       user authorization.
+    #
     # Returns true if there is at least one item matching the given
-    # attributes.
+    # attributes and search parameters.
     #
-    # Of the four types of key/value pairs that you can use when
-    # generating a search dictionary, you only need to give attributes
-    # in order for this method to work.
+    # This method provides a default class, and enforces a return type
+    # and match limit; the default key/value pairs:
     #
-    # The class this method assumes you are search for is Internet
-    # Password. You can overrid this by providing a different KSecClass
-    # value in the search dictionary.
+    # * KSecClass            => KSecClassInternetPassword
+    # * KSecMatchLimit       => KSecMatchLimitOne
+    # * KSecReturnAttributes => true
     #
-    # Normally, you will only need to add attributes of the keychain
-    # item to the search dictionary, such as KSecAttrServer (URL) and/or
-    # KSecAttrAccount (username). These are listed under "Attribute Item
-    # Keys and Values" in Apple's documentation.
+    # The class can be overridden, but the match limit and return type
+    # cannot. You can add as many attributes and/or search parameters
+    # as you like, they are listed under "Attribute Item Keys and
+    # Values" or "Search Keys" in Apple's documentation.
     #
-    # Optionally, you can also add search modifying key/value pairs
-    # which add other search constraints, such as case-insensitive
-    # searching (KSecMatchCaseInsensitive => true). These are located
-    # under the "Search Attribute Keys" section in Apple's documentation.
-    #
-    # Return type key/value pairs will be ignored; the only return type
-    # that makes sense for this method will automatically be added.
     # @param [Hash] search_dict
     # @raise [KeychainException] only for unexpected result codes
     def item_exists? search_dict
-      dict   = {
-        KSecClass            => KSecClassInternetPassword
-      }.merge! search_dict.merge(
-        KSecMatchLimit       => KSecMatchLimitOne,
-        KSecReturnAttributes => true
-      )
-      for key in [KSecReturnData, KSecReturnRef, KSecReturnPersistentRef]
-        dict.delete key
-      end
-      result = Pointer.new :id # we MUST pass a pointer; nil is not allowed
+      dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
+                                 { KSecMatchLimit => KSecMatchLimitOne },
+                                 search_dict,
+                                 KSecReturnAttributes )
 
-      case error_code = SecItemCopyMatching( dict, result )
-      when ErrSecSuccess then      true
+      result     = Pointer.new(:id)
+      error_code = SecItemCopyMatching( dict, result )
+
+      case error_code
+      when ErrSecSuccess      then true
       when ErrSecItemNotFound then false
       else
         raise KeychainException.new( 'Checking item existence', error_code )
@@ -85,6 +76,17 @@ module Keychain
       else
         raise KeychainException.new( 'Looking up item', error_code )
       end
+    end
+
+
+    private
+
+    def create_search_dict override, enforce, user, return_type
+      dict = override.merge(user).merge(enforce)
+      for key in [KSecReturnAttributes, KSecReturnData, KSecReturnRef, KSecReturnPersistentRef]
+        dict.delete key
+      end
+      dict.merge!( return_type => true )
     end
 
   end
