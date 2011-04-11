c91419201a4958f5363900f6a5d7b628ded0359d
Mark Rada <mrada@marketcircle.com>
2011-04-11 00:32:31 -0400

    Include the error code in exception messages

--- lib/mr_keychain/keychain_exception.rb
+++ lib/mr_keychain/keychain_exception.rb
@@ -9,7 +9,7 @@ class KeychainException < Exception
   # @param [Fixnum] error_code result code from calling a Sec function
   def initialize message_prefix, error_code
     cf_message = SecCopyErrorMessageString( error_code, nil )
-    super "#{message_prefix}. #{cf_message}"
+    super "#{message_prefix}. [Error code: #{error_code}] #{cf_message}"
   end
 
 end
