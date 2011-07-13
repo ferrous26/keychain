1d446b81a4faf31223922dd7fe31bc592d886bf7
Mark Rada <mrada@marketcircle.com>
2011-07-13 02:10:17 -0400

    Indent less, save trees

--- lib/mr_keychain/keychain.rb
+++ lib/mr_keychain/keychain.rb
@@ -1,119 +1,115 @@
-module Keychain
+class << Keychain
 
-  class << self
+  ##
+  # @note This method asks only for the metadata and should never need
+  #       user authorization.
+  #
+  # Returns true if there is at least one item matching the given
+  # attributes and search parameters.
+  #
+  # This method provides a default class, and enforces a return type
+  # and match limit; the default key/value pairs:
+  #
+  # * `{ KSecClass            => KSecClassInternetPassword }`
+  # * `{ KSecMatchLimit       => KSecMatchLimitOne }`
+  # * `{ KSecReturnAttributes => true }`
+  #
+  # The class can be overridden, but the match limit and return type
+  # cannot. You can add as many attributes and/or search parameters
+  # as you like, they are listed under "Attribute Item Keys and
+  # Values" or "Search Keys" in Apple's documentation.
+  #
+  # @param [Hash] search_dict
+  # @raise [KeychainException] only for unexpected result codes
+  def item_exists? search_dict
+    dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
+                               { KSecMatchLimit => KSecMatchLimitOne },
+                               search_dict,
+                               KSecReturnAttributes )
 
-    ##
-    # @note This method asks only for the metadata and should never need
-    #       user authorization.
-    #
-    # Returns true if there is at least one item matching the given
-    # attributes and search parameters.
-    #
-    # This method provides a default class, and enforces a return type
-    # and match limit; the default key/value pairs:
-    #
-    # * `{ KSecClass            => KSecClassInternetPassword }`
-    # * `{ KSecMatchLimit       => KSecMatchLimitOne }`
-    # * `{ KSecReturnAttributes => true }`
-    #
-    # The class can be overridden, but the match limit and return type
-    # cannot. You can add as many attributes and/or search parameters
-    # as you like, they are listed under "Attribute Item Keys and
-    # Values" or "Search Keys" in Apple's documentation.
-    #
-    # @param [Hash] search_dict
-    # @raise [KeychainException] only for unexpected result codes
-    def item_exists? search_dict
-      dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
-                                 { KSecMatchLimit => KSecMatchLimitOne },
-                                 search_dict,
-                                 KSecReturnAttributes )
+    result     = Pointer.new(:id)
+    error_code = SecItemCopyMatching( dict, result )
 
-      result     = Pointer.new(:id)
-      error_code = SecItemCopyMatching( dict, result )
-
-      case error_code
-      when ErrSecSuccess      then true
-      when ErrSecItemNotFound then false
-      else
-        raise KeychainException.new( 'Checking item existence', error_code )
-      end
+    case error_code
+    when ErrSecSuccess      then true
+    when ErrSecItemNotFound then false
+    else
+      raise KeychainException.new( 'Checking item existence', error_code )
     end
+  end
 
-    ##
-    # @note This method asks only for the metadata and should never need
-    #       user authorization.
-    #
-    # Returns a {Keychain::Item} if an item is found, otherwise returns nil.
-    #
-    # The hash argument for this method is constructed exactly the same as
-    # with {Keychain.item_exists?}, with the same default values given.
-    #
-    # @raise [KeychainException] only for unexpected result codes
-    # @param [Hash] search_dict
-    # @return [Keychain::Item,nil]
-    def item search_dict
-      dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
-                                 { KSecMatchLimit => KSecMatchLimitOne },
-                                 search_dict,
-                                 KSecReturnAttributes )
+  ##
+  # @note This method asks only for the metadata and should never need
+  #       user authorization.
+  #
+  # Returns a {Keychain::Item} if an item is found, otherwise returns nil.
+  #
+  # The hash argument for this method is constructed exactly the same as
+  # with {Keychain.item_exists?}, with the same default values given.
+  #
+  # @raise [KeychainException] only for unexpected result codes
+  # @param [Hash] search_dict
+  # @return [Keychain::Item,nil]
+  def item search_dict
+    dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
+                               { KSecMatchLimit => KSecMatchLimitOne },
+                               search_dict,
+                               KSecReturnAttributes )
 
-      result = Pointer.new(:id)
-      error_code = SecItemCopyMatching( dict, result )
+    result = Pointer.new(:id)
+    error_code = SecItemCopyMatching( dict, result )
 
-      case error_code
-      when ErrSecSuccess      then Item.new(result[0])
-      when ErrSecItemNotFound then nil
-      else
-        raise KeychainException.new( 'Looking up item', error_code )
-      end
+    case error_code
+    when ErrSecSuccess      then Item.new(result[0])
+    when ErrSecItemNotFound then nil
+    else
+      raise KeychainException.new( 'Looking up item', error_code )
     end
+  end
 
-    ##
-    # @note This method asks only for the metadata and should never need
-    #       user authorization.
-    #
-    # Returns an array of {Keychain::Item} objects, possibly empty.
-    #
-    # The hash argument for this method is constructed similarly to
-    # {Keychain.item_exists?}. The only difference is that you can call
-    # this method with no arguments and it will return all the keychain
-    # items that match the default search attributes (just the class type).
-    #
-    # This method also sets the return limit for you:
-    # * `KSecMatchLimit => KSecMatchLimitAll`
-    #
-    # @raise [KeychainException] only for unexpected result codes
-    # @param [Hash] search_dict
-    # @return [Array<Keychain::Item>]
-    def items search_dict = {}
-      dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
-                                 { KSecMatchLimit => KSecMatchLimitAll },
-                                 search_dict,
-                                 KSecReturnAttributes )
+  ##
+  # @note This method asks only for the metadata and should never need
+  #       user authorization.
+  #
+  # Returns an array of {Keychain::Item} objects, possibly empty.
+  #
+  # The hash argument for this method is constructed similarly to
+  # {Keychain.item_exists?}. The only difference is that you can call
+  # this method with no arguments and it will return all the keychain
+  # items that match the default search attributes (just the class type).
+  #
+  # This method also sets the return limit for you:
+  # * `KSecMatchLimit => KSecMatchLimitAll`
+  #
+  # @raise [KeychainException] only for unexpected result codes
+  # @param [Hash] search_dict
+  # @return [Array<Keychain::Item>]
+  def items search_dict = {}
+    dict = create_search_dict( { KSecClass => KSecClassInternetPassword },
+                               { KSecMatchLimit => KSecMatchLimitAll },
+                               search_dict,
+                               KSecReturnAttributes )
 
-      result = Pointer.new(:id)
-      error_code = SecItemCopyMatching( dict, result )
+    result = Pointer.new(:id)
+    error_code = SecItemCopyMatching( dict, result )
 
-      case error_code
-      when ErrSecSuccess      then result[0].map { |item| Item.new(item) }
-      when ErrSecItemNotFound then []
-      else
-        raise KeychainException.new( 'Looking up item', error_code )
-      end
+    case error_code
+    when ErrSecSuccess      then result[0].map { |item| Item.new(item) }
+    when ErrSecItemNotFound then []
+    else
+      raise KeychainException.new( 'Looking up item', error_code )
     end
+  end
 
 
-    private
+  private
 
-    def create_search_dict override, enforce, user, return_type
-      dict = override.merge(user).merge(enforce)
-      for key in [KSecReturnAttributes, KSecReturnData, KSecReturnRef, KSecReturnPersistentRef]
-        dict.delete key
-      end
-      dict.merge!( return_type => true )
+  def create_search_dict override, enforce, user, return_type
+    dict = override.merge(user).merge(enforce)
+    for key in [KSecReturnAttributes, KSecReturnData, KSecReturnRef, KSecReturnPersistentRef]
+      dict.delete key
     end
-
+    dict.merge!( return_type => true )
   end
 
 end
